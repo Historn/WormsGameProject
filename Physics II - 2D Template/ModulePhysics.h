54e305c85109275f7c369dc2f3fa7ef695b3d3d1
Historn <arnaujiga@gmail.com>
2022-12-27 14:08:20 +0100

    Update ModulePhysics.h

--- Physics II - 2D Template/ModulePhysics.h
+++ Physics II - 2D Template/ModulePhysics.h
@@ -74,12 +74,6 @@ public:
 	
 	float angle;
 
-	/// The body type.
-	/// static: zero mass, zero velocity, may be manually moved
-	/// kinematic: zero mass, non-zero velocity set by user, moved by solver
-	/// dynamic: positive mass, non-zero velocity determined by forces, moved by solver
-	BodyType bType;
-
 	// Has physics enabled?
 	bool physics_enabled = true;
 };
